ea1582aa73436177c5c543da74a8419412a36684
Basia <b.kumor@yahoo.com>
2020-08-27 19:08:12 +0200

    global vars

---
+++ app.js
@@ -0,0 +1,9 @@
+const squares = document.querySelectorAll('.board div');
+const currentPlayer = document.getElementById('current-player');
+const winner = document.getElementById('winner');
+
+for (let square of squares) {
+    square.addEventListener('click', function() {
+        
+    })
+}
