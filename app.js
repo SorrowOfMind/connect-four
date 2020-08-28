b735525d09a9738092b55eb21c352d194eaa332c
Basia <b.kumor@yahoo.com>
2020-08-28 18:58:02 +0200

    swap players, assign squares

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const squares = document.querySelectorAll('.board div');
-const currentPlayer = document.getElementById('current-player');
+const currentPlayerDisplay = document.getElementById('current-player');
 const winner = document.getElementById('winner');
+const ROW_SIZE = 7;
+let currentPlayer = 1;
 
-for (let square of squares) {
-    square.addEventListener('click', function() {
+squares.forEach((square, idx) => square.addEventListener('click', () => manageSquares(idx)));
+
+const assignSquare = (sq, playerTag, player) => {
+    sq.classList.add('occupied');
+    sq.classList.add(playerTag)
+    currentPlayer = player;
+    currentPlayerDisplay.innerText = currentPlayer;
+}
+
+function manageSquares (idx) {
+    if (square[idx + ROW_SIZE].classList.contains('occupied')) {
+        if (currentPlayer === 1) assignSquare(square[idx], 'player-one', 2);
+        else assignSquare(square[idx], 'player-two', 1);
+    } else {
         
-    })
+    }
 }
